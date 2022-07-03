c21a360592458c9b33e05a2de6f8597e713bef3a
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2022-07-03 14:37:45 +0300

    update Main 2

---
+++ src/homeWork2/Alishev2.java
@@ -0,0 +1,26 @@
+package homeWork2;
+public class Alishev2 {
+    //3 уроки алишева
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"Niki");
+        System.out.println("My number is "+ x);
+        System.out.println("Следующий урок:===>");
+        // 4 уроки алишева
+        boolean t = 5==5;
+        System.out.println(t);
+        int value = 0;
+        while(value<5){
+            System.out.println("Hello "+ value);
+            value = value +1;}
+        int hillellesson = 0;
+        while (hillellesson<16){
+            System.out.println("Я не знаю JAVA");
+            hillellesson = hillellesson + 1;}
+        System.out.println("Я знаю JAVA...не очень");
+    }
+}
