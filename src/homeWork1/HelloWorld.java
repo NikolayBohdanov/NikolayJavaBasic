89f56824f1cc646e5f545a94fff6c60508e8ee0b
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2022-07-03 14:33:25 +0300

    main Update

---
+++ src/homeWork1/HelloWorld.java
@@ -0,0 +1,21 @@
+package homeWork1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("From");
+        System.out.print("Nikolay ");
+        System.out.println("Bohdanov");
+        System.out.print("Nice ");
+        System.out.println("to meet you.");
+        System.out.println("The weather is fine");
+        System.out.println("Who is on duty today?");
+        System.out.println("Kto duet? Ya ne duyou");
+        System.out.println("BB 11 01 B9 0D 00 B4 0E 8A 07 43 CD 10 E2 F9 CD 20 48 65 6C 6C 6F 2C 20 57 6F 72 6C 64 21");
+        System.out.println("Goodbye");
+
+    }
+
+
+}
