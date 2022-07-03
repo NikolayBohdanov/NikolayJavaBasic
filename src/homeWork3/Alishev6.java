210722678d05f54dea25b86550471b8b70054b90
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2022-07-03 14:42:47 +0300

    main Update3

---
+++ src/homeWork3/Alishev6.java
@@ -0,0 +1,13 @@
+public class Alishev6 {
+    public static void main(String[] args) {
+        int a = 5;
+        if (a > 7) {
+            System.out.println("я сдам дз вовремя");
+        } else if (a < 5)
+            System.out.println("no possible");
+    }else
+
+    {
+        System.out.println("Ни один из возможных условий не реализовано");
+    }
+}
