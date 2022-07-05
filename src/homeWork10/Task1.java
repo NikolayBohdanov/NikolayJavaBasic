fcc6c57cb4318cc1c7b466956bdbf282c16d7c54
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2022-07-05 22:33:57 +0300

    Update main with third part of homeWork 10

--- src/homeWork10/Task1.java
+++ src/homeWork10/Task1.java
@@ -10,38 +10,40 @@ public class Task1 {
         max(twenty);
         min(twenty);
     }
-        public static void max (int[]twenty) {
+
+    public static void max(int[] twenty) {
         int maxValue = twenty[0];
-        int numberMaxValues=0;
-        for(int i = 1; i<twenty.length; i++) {
-            if(twenty[i]>maxValue){
+        int numberMaxValues = 0;
+        for (int i = 1; i < twenty.length; i++) {
+            if (twenty[i] > maxValue) {
                 maxValue = twenty[i];
             }
         }
-            for (int i : twenty) {
-                if (i == maxValue) {
-                    numberMaxValues++;
-                }
+        for (int i : twenty) {
+            if (i == maxValue) {
+                numberMaxValues++;
             }
-            System.out.println("Maximum value of this array = "+ maxValue);
-            System.out.println("There are "+ numberMaxValues+ " element(s) with maximum value");
-
         }
-        public static void min (int[]twenty) {
-            int minValue = twenty[0];
-            int numberMinValues=0;
-            for(int i = 1; i<twenty.length; i++) {
-                if(twenty[i]<minValue){
-                    minValue = twenty[i];
-                }
-            }
-            for (int i : twenty) {
-                if (i == minValue) {
-                    numberMinValues++;
-                }
-            }
-            System.out.println("Minimum value of this array = "+ minValue);
-            System.out.println("There are "+ numberMinValues+ " element(s) with minimum value");
+        System.out.println("Maximum value of this array = " + maxValue);
+        System.out.println("There are " + numberMaxValues + " element(s) with maximum value");
 
-        }
     }
+
+    public static void min(int[] twenty) {
+        int minValue = twenty[0];
+        int numberMinValues = 0;
+        for (int i = 1; i < twenty.length; i++) {
+            if (twenty[i] < minValue) {
+                minValue = twenty[i];
+            }
+        }
+        for (int i : twenty) {
+            if (i == minValue) {
+                numberMinValues++;
+            }
+        }
+        System.out.println("Minimum value of this array = " + minValue);
+        System.out.println("There are " + numberMinValues + " element(s) with minimum value");
+
+    }
+}
--- src/homeWork10/Task2.java
+++ src/homeWork10/Task2.java
@@ -1,20 +1,41 @@
 package homeWork10;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        String[] feelings = new String[]{"anger", "awe", "joy", "love", "grief"};
-        Scanner letters = new Scanner(System.in);
-        int howMany = 0;
+        String[] feelings = {"anger", "awe", "joy", "love", "grief"};
         System.out.println("Please input random symbols:");
-        String randomLetters = letters.nextLine();
-        for (int i = 0; i < feelings.length; i++) {
-            if (feelings[i].contentEquals(randomLetters)) {
-                howMany++;
+        Scanner symbols = new Scanner(System.in);
+        String s = symbols.nextLine();
+        char[] lettersConsol = s.toCharArray();
+        System.out.println(Arrays.toString(lettersConsol));
+        int[] a = getLetter(s);
+        int count = 0;
+        for (String word : feelings) {
+            int[] wordFreq = getLetter(word);
+            boolean ok = true;
+            for (int i = 0; ok && i < a.length; i++) {
+                if (wordFreq[i] > a[i]) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                count++;
             }
         }
-        System.out.println(randomLetters + "==" + howMany + "feelings");
+        System.out.println("You can make " + count + " feelings");
+        symbols.close();
+    }
+
+    public static int[] getLetter(String word) {
+        int[] a = new int[26];
+        for (char c : word.toLowerCase().toCharArray()) {
+            if (Character.isLetter(c)) {
+                a[c - 'a']++;
+            }
+        }
+        return a;
     }
 }
-
