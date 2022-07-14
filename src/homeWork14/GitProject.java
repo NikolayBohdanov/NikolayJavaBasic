fa291f12d8f23df42b3d1fcf7605f93c76313703
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2022-07-14 16:34:34 +0300

    Upload homeWork 14(git)

---
+++ src/homeWork14/GitProject.java
@@ -0,0 +1,44 @@
+package homeWork14;
+
+import java.util.Scanner;
+
+public class GitProject {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        System.out.println("Hi, please input value of your bet and risk coefficient:");
+        System.out.println("Bet value: ");
+        int a = user.nextInt();
+        System.out.println("Risk value: ");
+        double b = user.nextDouble();
+        Bet userBet = new Bet();
+        userBet.setBet(a);
+        userBet.setRisk(b);
+        System.out.println("Your bet value: " + userBet.getBet());
+        System.out.println("Your bet value: " + userBet.getRisk());
+        user.close();
+    }
+}
+
+class Bet {
+    private int bet;
+    private double risk;
+
+    public Bet() {
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public void setRisk(double risk) {
+        this.risk = risk;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public double getRisk() {
+        return risk;
+    }
+}
